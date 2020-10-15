3aed5557fb180b0fa36afafb84a468e723beb4e9
Brandon <brandon.startz@gmail.com>
2020-10-15 09:25:23 +0300

    Moved audio mixer, added intro scene, more audio fixes

---
+++ GP_Asteroids/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (!this.GetComponent<VideoPlayer>().isPlaying)
+        {
+            // print("Playing");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/Gameplay");
+        }
+    }
+}
